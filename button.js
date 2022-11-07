c34660a21241cca7b4696d6d13fa0d8255dec5f9
sancheyo <113383365+sancheyo@users.noreply.github.com>
2022-11-07 09:43:24 -0500

    Add files via upload

---
+++ button.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    const myWindow = window.open("YYSanchez_Resume-project.pdf", "YYSanchez_Resume-project.pd", "width=300,height=300");
+    myWindow.opener.document.getElementById().innerHTML = "HELLO";
+  }
